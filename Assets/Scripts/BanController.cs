049faf42aee5b68d7c9f4a6beb3548d2227835a0
antgit <iljinaa@gmail.com>
2015-12-16 15:43:25 +0200

    v1

---
+++ Assets/Scripts/BanController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BanController : MonoBehaviour {
+	public Transform ban;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		newPos.z = -0.1f;
+		ban.position = newPos;
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+		if (Input.GetMouseButton (0)) {
+			Destroy(coll.gameObject);
+		}
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Enemy")
+			coll.gameObject.SendMessage("ApplyDamage", 10);
+	}
+}
---
+++ Assets/Scripts/DickController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DickController : MonoBehaviour {
+	private float InstantiationTimer = 1f;
+	public Transform prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InstantiationTimer -= Time.deltaTime;
+		if (InstantiationTimer <= 0)
+		{
+			var x = Random.Range(-8, 8);
+			var y = Random.Range(-3, 3);
+			Instantiate(prefab, new Vector3(x,y,0), Quaternion.identity);
+			InstantiationTimer = 1f;
+		}
+	}
+}
